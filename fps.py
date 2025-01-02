4ce36b077ceab56ddf3e12764663cad20918e2dd
arcadie99 <arcadie0307@gmail.com>
2025-01-02 14:03:05 +0200

    make text working and add the fps and score status

---
+++ fps.py
@@ -0,0 +1,12 @@
+import pygame
+
+class Text:
+    def __init__(self):
+        pass
+
+    def draw(self, fps, screen, x = 40,y = 40, color = (255,255,255)): 
+        font = pygame.font.Font(None, 24)  # None uses the default font, 36 is the size
+        text_surface = font.render(fps, True, color)
+        text_rect = text_surface.get_rect(center=(x, y)) 
+
+        screen.blit(text_surface, text_rect)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from player import Player
 from asteroidfield import AsteroidField
 from asteroid import Asteroid
 from shot import Shot
+from fps import Text
 
 
 def main():
@@ -11,14 +12,20 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    enemyKills = 0;
+
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+
+    font = pygame.font.Font(None, 36)  # None uses the default font, 36 is the size
+    # Text content, color, and optional anti-aliasing
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
     asteroid = Asteroid(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, ASTEROID_MIN_RADIUS)
 
+    textObject = Text()
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
@@ -35,16 +42,14 @@ def main():
     Player.containers = (updatable, drawable)
     Shot.containers = (shots, updatable, drawable)
 
- 
     AsteroidField.containers = (updatable,)
     asteroid_field = AsteroidField()
-
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+ 
         screen.fill((0, 0, 0))
 
         for sprite in drawable:
@@ -57,16 +62,21 @@ def main():
             for shot in shots:
                 if asteroid.detectShotColision(shot):
                     asteroid.split()
+                    enemyKills = enemyKills  + 1
                     shot.kill()
                     break
 
             if asteroid.detectColision(player):
                 print("Game Over!")
 
+        text = f"Score: {enemyKills}"
+        textObject.draw(text,screen)
+
+        text = f"{clock.get_fps():2.0f} FPS"
+        textObject.draw(text, screen,  40, 80, color=(255,0,0))
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000  # Convert milliseconds to seconds
-        print(f"FPS: {clock.get_fps():.2f}")
 
 if __name__ == "__main__":
         main()
